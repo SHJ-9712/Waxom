503afb136f4b98040a70a27b0689be1d50a5553b
ShinHyukJun <xpqmf123@naver.com>
2020-12-14 18:23:05 +0900

    .main-visual .slogan 애니메이션 추가

--- js/slider.js
+++ js/slider.js
@@ -1,2 +1,2 @@
 // 상단 메인 슬라이더
-$('.main-visual').slick({infinite : true, pauseOnHover : false, dots: true, arrows : true, autoplay: true, autoplaySpeed: 2500});
+$('.main-visual').slick({infinite : true, pauseOnHover : false, pauseOnFocus: false, dots: true, arrows : true, autoplay: true, autoplaySpeed: 2500});
