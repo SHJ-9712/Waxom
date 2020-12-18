ad21481e5daa6143ab1f1a2e22c510b7ed301dda
ShinHyukJun <xpqmf123@naver.com>
2020-12-18 18:37:39 +0900

    .video 모달창 js 수정

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-// .video 동영상 모달창
+// 상단 메인 슬라이더
+$('.main-visual').slick({
+    infinite : true,
+    pauseOnHover : false,
+    pauseOnFocus: false,
+    dots: true,
+    arrows : true,
+    autoplay: true,
+    autoplaySpeed: 2500
+});
+
+// .video 모달창
 const playBtn = document.querySelector(".play-icon"),
     overlay = document.querySelector(".overlay"),
     closeBtn = document.querySelector(".close");
-
 const openModal = () => {
     overlay.style.display = "block";
 }
 const closeModal = () => {
     overlay.style.display = "none";
+    $(".overlay iframe").attr("src", $(".overlay iframe").attr("src"));
 }
 overlay.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
