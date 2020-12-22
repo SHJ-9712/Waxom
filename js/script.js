d5f57783888331dd5cb4d30ad7a33ed3b1d1c886
ShinHyukJun <xpqmf123@naver.com>
2020-12-22 21:04:24 +0900

    .latest-projects 카테고리 제이쿼리 완성

--- js/script.js
+++ js/script.js
@@ -9,6 +9,47 @@ $('.main-visual').slick({
     autoplaySpeed: 2500
 });
 
+// .latest-projects 카테고리
+$('.categories > button').click(function(){
+    $('.categories > button').removeClass('active')
+    $(this).addClass('active')
+});
+
+$('.all').click(function(){
+    $('.project-list > li').fadeIn(500);    
+});
+$('.webDesign').click(function(){
+    $('.project-list > .wD').fadeIn(500);
+    $('.project-list .mA, .illu, .ps').fadeOut(500);
+});
+$('.mobileApp').click(function(){
+    $('.project-list > .mA').fadeIn(500);
+    $('.project-list .wD, .illu, .ps').fadeOut(500);
+});
+$('.illust').click(function(){
+    $('.project-list > .illu').fadeIn(500);
+    $('.project-list .wD, .mA, .ps').fadeOut(500);
+});
+$('.psd').click(function(){
+    $('.project-list > .ps').fadeIn(500);
+    $('.project-list .wD, .mA, .illu').fadeOut(500);
+});
+
+// .video 모달창
+const playBtn = document.querySelector('.play-icon'),
+    overlay = document.querySelector('.overlay'),
+    closeBtn = document.querySelector('.close');
+const openModal = () => {
+    overlay.style.display = 'block';
+}
+const closeModal = () => {
+    overlay.style.display = 'none';
+    $('.overlay iframe').attr('src', $('.overlay iframe').attr('src'));
+}
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+playBtn.addEventListener('click', openModal);
+
 // .recent-posts 슬라이더
 $('.posts-list').slick({
     infinite: true,
@@ -19,18 +60,3 @@ $('.posts-list').slick({
     prevArrow: $('.prev'),
     nextArrow: $('.next')
 });
-
-// .video 모달창
-const playBtn = document.querySelector(".play-icon"),
-    overlay = document.querySelector(".overlay"),
-    closeBtn = document.querySelector(".close");
-const openModal = () => {
-    overlay.style.display = "block";
-}
-const closeModal = () => {
-    overlay.style.display = "none";
-    $(".overlay iframe").attr("src", $(".overlay iframe").attr("src"));
-}
-overlay.addEventListener("click", closeModal);
-closeBtn.addEventListener("click", closeModal);
-playBtn.addEventListener("click", openModal);
